0f4a51118f8e59c7b73af931e322fd9021d408f8
Guillaume Macbook <g.dharcourt+github@posteo.net>
2022-02-10 17:14:42 +0100

    création fonction "renomer calque"

--- myScript/Mutt comp translator/Script intermédiaire/renommer calque.jsx
+++ myScript/Mutt comp translator/Script intermédiaire/renommer calque.jsx
@@ -8,18 +8,9 @@ var layer = comp.layer(1);
 
 
 function renameLayer(input) {
-   
+
     layer.name = input;
     alert("renameLayer done");
-    //     var name = basename + ' ' + String(i + 1);
-    //     var currLayer = curComp.selectedLayers[i];
-    //     try{
-    //       currLayer.source.name = name;
-    //     }catch(error) {
-    //       $.writeln('this layer has no source');
-    //     }
-    //     currLayer.name = name;
-    //   }
 }
 
 startButton.onClick = renameLayer("Calque renommé");
