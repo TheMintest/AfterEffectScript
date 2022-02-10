832406e8738df2aa3e9b9156de99020a1d81edb8
Guillaume Macbook <g.dharcourt+github@posteo.net>
2022-02-10 17:19:57 +0100

    création de fonction pour renommer et remplacer le texte d'un calque

---
+++ myScript/Mutt comp translator/Script intermédiaire/renommer et remplacer texte.jsx
@@ -0,0 +1,30 @@
+var window = new Window("palette", "My script", undefined);
+var startButton = window.add("button", undefined, "Run !");
+
+window.show();
+
+var comp = app.project.activeItem;
+var layer = comp.layer(1);
+
+//remplace le contenue d'un calque de texte par un string hardcodé à l'interieur de la fonction 
+function replaceText(input) {
+
+    layer.property("Source Text").setValue(input);
+
+    alert("Replace text done");
+}
+
+function renameLayer(input) {
+
+    layer.name = input;
+    alert("renameLayer done");
+}
+
+startButton.onClick = function() {
+    var input = "Mon texte remplacé";
+    
+    renameLayer(input);
+    replaceText(input);
+
+    alert("Main fonction done");
+}
