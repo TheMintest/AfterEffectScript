fed0e066b8173ca765fdbe1d188385cf5048fe8d
Guillaume Macbook <g.dharcourt+github@posteo.net>
2022-02-11 11:14:26 +0100

    modificatoin de .gitignore, finalisation de "remplacer et renommer plusieurs calque"

---
+++ myScript/Mutt comp translator/Script intermédiaire/renommer et remplacer texte multiple.jsx
@@ -0,0 +1,37 @@
+var window = new Window("palette", "rename and replace multiple layer", undefined);
+var startButton = window.add("button", undefined, "Run !");
+
+window.show();
+
+var layerNumber;
+var comp = app.project.activeItem;
+var input = ""
+
+function renameLayer(selectedLayer, input) {
+    selectedLayer.name = input;
+    alert("renameLayer done");
+}
+
+function replaceText(selectedLayer, input) {
+    selectedLayer.property("Source Text").setValue(input);
+    alert("Replace text done");
+}
+
+startButton.onClick = function() {
+    var numberOfLayer = comp.layers.length;
+    alert("there's "+numberOfLayer+" layer in the comp");
+    for (i=1; i<numberOfLayer; i++) {
+        var selectedLayer = comp.layer(i);
+        alert("current selected layer is " + selectedLayer.name);
+        input = "texte replacé " + i;
+
+        if (selectedLayer.property("Source Text")!=null){
+            alert("selected layer is a text layer");
+            // replaceText("Test multiple calque"+i);
+            replaceText(selectedLayer, input);
+            renameLayer(selectedLayer, input);            
+        } else {
+            alert("no more text layer in the current comp");
+        }
+    }
+}
