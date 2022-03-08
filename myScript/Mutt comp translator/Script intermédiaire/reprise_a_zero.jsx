3a713469e0a61426b8803ce96a0eb197920c1b3c
Guillaume Macbook <g.dharcourt+github@posteo.net>
2022-03-08 16:29:43 +0100

    On abandonne ce fichier-ci

--- myScript/Mutt comp translator/Script intermédiaire/reprise_a_zero.jsx
+++ myScript/Mutt comp translator/Script intermédiaire/reprise_a_zero.jsx
@@ -1,36 +1,34 @@
-const aeq = require("aequery");
-
 //dépendance : 
 #include '/Users/guillaumedharcourt/node_modules/aequery/dist/aequery.js' // aequery is now available as aeq
-// Disable all Camera lens blur effects in active comp
-aeq( 'activecomp effect[matchName="ADBE Camera Lens Blur"]' ).attr( 'enabled', false );
+    // Disable all Camera lens blur effects in active comp
+aeq('activecomp effect[matchName="ADBE Camera Lens Blur"]').attr('enabled', false);
 
 // MAINWINDOW
 
 // ==========
-var mainWindow = new Window("palette", undefined, undefined, {minimizeButton: true, resizeable: true}); 
-    mainWindow.text = "MUTT's Auto CompTranslator"; 
-    mainWindow.orientation = "column"; 
-    mainWindow.alignChildren = ["center","top"]; 
-    mainWindow.spacing = 10; 
-    mainWindow.margins = 16; 
+var mainWindow = new Window("palette", undefined, undefined, { minimizeButton: true, resizeable: true });
+mainWindow.text = "MUTT's Auto CompTranslator";
+mainWindow.orientation = "column";
+mainWindow.alignChildren = ["center", "top"];
+mainWindow.spacing = 10;
+mainWindow.margins = 16;
 
 // GROUPONE
 // ========
-var groupOne = mainWindow.add("group", undefined, {name: "groupOne"}); 
+var groupOne = mainWindow.add("group", undefined, { name: "groupOne" });
 
-var fileLocBox = groupOne.add('edittext {properties: {name: "fileLocBox"}}'); 
-    fileLocBox.text = "Fichier ?"; 
+var fileLocBox = groupOne.add('edittext {properties: {name: "fileLocBox"}}');
+fileLocBox.text = "Fichier ?";
 
-var getFileButton = groupOne.add("button", undefined, undefined, {name: "getFileButton"}); 
-    getFileButton.text = "Fichier..."; 
-    getFileButton.helpTip="Selectionner l'emplacement du fichier CSV"
+var getFileButton = groupOne.add("button", undefined, undefined, { name: "getFileButton" });
+getFileButton.text = "Fichier...";
+getFileButton.helpTip = "Selectionner l'emplacement du fichier CSV"
 
 // GROUPTWO
 // ========
-var groupTwo = mainWindow.add("group", undefined, {name: "groupTwo"}); 
-var applyButton = groupTwo.add("button", undefined, undefined, {name: "applyButton"}); 
-    applyButton.text = "C'est parti !"; 
+var groupTwo = mainWindow.add("group", undefined, { name: "groupTwo" });
+var applyButton = groupTwo.add("button", undefined, undefined, { name: "applyButton" });
+applyButton.text = "C'est parti !";
 
 //global variable
 var selectedFile = new File;
@@ -38,7 +36,7 @@ var check = 0;
 var currentLanguageName;
 var csvData = [];
 var comp = app.project.activeItem;
-var textNumberLine; 
+var textNumberLine;
 var selectedLayer;
 var numberOfLanguage;
 var dossierFr = findItemByName("Français");
@@ -48,9 +46,9 @@ var numberOfComp = dossierFr.numItems;
 
 //Script Start
 
- mainWindow.show();
+mainWindow.show();
 
-  //selection du fichier
+//selection du fichier
 getFileButton.onClick = function() {
     selectedFile = selectedFile.openDlg("Please select a file", "Acceptable file : *.csv")
     fileLocBox.text = selectedFile.fsName;
@@ -61,29 +59,27 @@ getFileButton.onClick = function() {
 app.beginUndoGroup("appliquer le script");
 
 applyButton.onClick = function() {
-    if(check === 0) {
+    if (check === 0) {
         alert("Please select a file");
     } else {
         readCSV();
 
-        numberOfLanguage = csvData.length-1;
+        numberOfLanguage = csvData.length - 1;
 
         // inserer boucle du nombre de language
-        for (var currentLanguage =2; currentLanguage <= numberOfLanguage; currentLanguage++) {
+        for (var currentLanguage = 2; currentLanguage <= numberOfLanguage; currentLanguage++) {
 
             //créer un array qui contient tout nos texte
             var currentLineArray = csvData[currentLanguage].split(",");
 
             // Crée un dossier nommé comme la langue de la ligne actuelle et le stock dans une variable
-            var currentLanguageFolder = app.project.items.addFolder(currentLineArray[0]);
+            aeq.project.getOrCreateFolder(currentLineArray[0]);
 
 
-            for(var frComp = 1; frComp<= dossierFr.numItems; frComp++) {
-                dossierFr.item(frComp).parentFolder = currentLanguageFolder;
-            }
+
 
             // for (var currentComp = 1; currentComp<=dossierFr.items.lenght)
-            
+
             // selectComp();
             // duplicateComp();
             // renameComp();
@@ -108,10 +104,10 @@ function readCSV() {
     do {
 
         csvData.push(selectedFile.readln());
-    } while(!selectedFile.eof);
+    } while (!selectedFile.eof);
     // alert("CSV lenght is" + csvData.length)
 }
 
 function findItemByName(myName) {
-    aeq.project.get
+    return aeq.project.getFolder(myName);
 }
